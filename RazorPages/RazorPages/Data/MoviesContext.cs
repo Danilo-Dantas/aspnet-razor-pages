6d7e1fd743d8c85422e34acef9a5dbdd2504d4fc
Danilo Dantas <danilodantasofc@gmail.com>
2025-03-02 00:55:08 -0300

    Feat: Instalando Pacotes do EntityFramework com Db In Memory e configurando valor padrão para a tabela

---
+++ RazorPages/RazorPages/Data/MoviesContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Slugify;
+
+namespace RazorPages.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var slugfy = new SlugHelper();
+
+            string[] names = [
+                "O Poderoso Chefão",
+                "O Senhor dos Aneis: O Retorno Do Rei",
+                "Interestelar",
+                "Cidadão Kane",
+                "O Silêncio dos Inocentes",
+                "Matrix",
+                "A Lista de Schindler",
+                "Forrest Gump",
+                "Star Wars: Episódio V - O Império Contra-Ataca",
+                "Pump Fiction: Tempo de Violência"
+            ];
+
+            int counter = 1;
+            var movies = new List<Movie>();
+            foreach (string name in names)
+            {
+                movies.Add(new Movie
+                {
+                    Id = counter,
+                    Name = name,
+                    permalink = slugfy.GenerateSlug(name)
+                });
+                counter++;
+            }
+
+            modelBuilder
+                .Entity<Movie>()
+                .HasData(movies);
+
+
+        }
+    }
+}
--- RazorPages/RazorPages/Program.cs
+++ RazorPages/RazorPages/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MoviesContext>(option => 
+    option.UseInMemoryDatabase("MyFirstAppDB"));
 
 var app = builder.Build();
 
