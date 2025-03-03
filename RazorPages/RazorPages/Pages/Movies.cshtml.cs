8c65120500a99d91595497f36577a42439ed3e80
Danilo Dantas <danilodantasofc@gmail.com>
2025-03-03 15:18:52 -0300

    Feat: Criando pasta Shared e Layout das páginas

--- RazorPages/RazorPages/Pages/Movies.cshtml.cs
+++ RazorPages/RazorPages/Pages/Movies.cshtml.cs
@@ -5,11 +5,16 @@ namespace RazorPages.Pages
 {
     public class MoviesModel(MoviesContext moviesContext) : PageModel
     {
-
         public ICollection<Movie> Movies { get; private set; }
+
         public void OnGet()
         {
             Movies = moviesContext.Movies.ToList();
         }
+
+        //public async Task OnGetAsync()
+        //{
+        //    Movies = await moviesContext.Movies.ToListAsync();
+        //}
     }
 }
